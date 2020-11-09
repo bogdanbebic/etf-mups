c7937a856ec02b3c26898cca364b57520814dfb3
Uros Krstic <uros.krstic98@gmail.com>
2020-11-09 18:01:32 +0100

    Implemented manual omp parallelization for calculating PI

--- openmp/dz1z1.c
+++ openmp/dz1z1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
 
 void Usage(char *prog_name);
 
@@ -10,22 +11,51 @@ void Usage(char *prog_name);
 
 int main(int argc, char *argv[])
 {
-    long long n, i;
+    long long n, i, thread_cnt;
     double factor;
     double sum = 0.0;
 
-    if (argc != 2)
+    // revert back to argc != 2
+    // after testing
+    if (argc != 3)
         Usage(argv[0]);
     n = strtoll(argv[1], NULL, 10);
+    thread_cnt = strtoll(argv[2], NULL, 10);
+
     if (n < 1)
         Usage(argv[0]);
 
     printf("Before for loop, factor = %f.\n", factor);
-    for (i = 0; i < n; i++)
+
+    double last_iteration_factor;
+    omp_set_num_threads(thread_cnt);
+
+#pragma omp parallel reduction(+: sum) private(factor, i) shared(last_iteration_factor)
+{
+    long long chunk = n / omp_get_num_threads();
+    long long start_i = omp_get_thread_num() * chunk; 
+    long long end_i = start_i + chunk;
+    int is_last_chunk = 0;
+
+    if (omp_get_thread_num() + 1 == omp_get_num_threads())
+    {
+        end_i += n % omp_get_num_threads();
+        is_last_chunk = 1;
+    }
+    // for debug purposes
+    // printf("id: %2d, start: %10lld, end: %11lld, chunk: %10lld\n", omp_get_thread_num(), start_i, end_i, chunk);
+
+    for (i = start_i; i < end_i; i++)
     {
         factor = (i % 2 == 0) ? 1.0 : -1.0;
         sum += factor / (2 * i + 1);
     }
+
+    // substitute for lastprivate(factor)
+    if (is_last_chunk) last_iteration_factor = factor;
+} // parallel
+
+    factor = last_iteration_factor;
     printf("After for loop, factor = %f.\n", factor);
 
     sum = 4.0 * sum;
