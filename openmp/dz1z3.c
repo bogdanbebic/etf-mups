038ea6eb47f6eb960728dc04f53921c9c4aa6d16
Uros Krstic <uros.krstic98@gmail.com>
2020-11-18 16:42:58 +0100

    openmp/dz1z3.c: Combine sequential and parallel executions

--- openmp/dz1z3.c
+++ openmp/dz1z3.c
@@ -5,11 +5,40 @@
 
 void Usage(char *prog_name);
 
+#define ACCURACY 0.01
+
 /*
  * tasks
  */
 
-int main(int argc, char *argv[])
+double sequential_solution(int argc, char *argv[])
+{
+    long long n, i;
+    double factor;
+    double sum = 0.0;
+
+    if (argc != 2)
+        Usage(argv[0]);
+    n = strtoll(argv[1], NULL, 10);
+    if (n < 1)
+        Usage(argv[0]);
+
+    printf("Before for loop, factor = %f.\n", factor);
+    for (i = 0; i < n; i++)
+    {
+        factor = (i % 2 == 0) ? 1.0 : -1.0;
+        sum += factor / (2 * i + 1);
+    }
+    printf("After for loop, factor = %f.\n", factor);
+
+    sum = 4.0 * sum;
+    printf("With n = %lld terms\n", n);
+    printf("   Our estimate of pi = %.14f\n", sum);
+    printf("   Ref estimate of pi = %.14f\n", 4.0 * atan(1.0));
+    return sum;
+}
+
+double parallel_solution(int argc, char *argv[])
 {
     long long n, i;
     double factor;
@@ -60,6 +89,29 @@ int main(int argc, char *argv[])
     printf("With n = %lld terms\n", n);
     printf("   Our estimate of pi = %.14f\n", sum);
     printf("   Ref estimate of pi = %.14f\n", 4.0 * atan(1.0));
+    return sum;
+}
+
+int main(int argc, char *argv[])
+{
+    printf("---------------------Sequential execution---------------------\n");
+    double start_time_seq = omp_get_wtime();
+    double sum_seq = sequential_solution(argc, argv);
+    double end_time_seq = omp_get_wtime();
+
+    printf("----------------------Parallel execution----------------------\n");
+    double start_time_parallel = omp_get_wtime();
+    double sum_parallel = parallel_solution(argc, argv);
+    double end_time_parallel = omp_get_wtime();
+
+    printf("\nSequential elapsed time: %lfs\n", end_time_seq - start_time_seq);
+    printf("Parallel elapsed time: %lfs\n", end_time_parallel - start_time_parallel);
+
+    if (fabs(sum_seq - sum_parallel) < ACCURACY)
+        printf("Test PASSED\n");
+    else
+        printf("Test FAILED\n");
+
     return 0;
 }
 
