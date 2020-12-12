b9d07aac7e123073dc0c182864e3461c56b38e5e
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-12-12 11:58:11 +0100

    mpi: Add pi_parallel implementation

--- mpi/pi_parallel.c
+++ mpi/pi_parallel.c
@@ -1,6 +1,13 @@
+#include <mpi.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+enum Constants
+{
+    MANAGER = 0,
+};
 
 void Usage(char *prog_name);
 
@@ -10,24 +17,56 @@ int main(int argc, char *argv[])
     double factor;
     double sum = 0.0;
 
-    if (argc != 2)
-        Usage(argv[0]);
-    n = strtoll(argv[1], NULL, 10);
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == MANAGER)
+    {
+        if (argc != 2)
+            Usage(argv[0]);
+        n = strtoll(argv[1], NULL, 10);
+    }
+
+    MPI_Bcast(&n, 1, MPI_LONG_LONG_INT, MANAGER, MPI_COMM_WORLD);
+
     if (n < 1)
         Usage(argv[0]);
 
-    printf("Before for loop, factor = %f.\n", factor);
-    for (i = 0; i < n; i++)
+    if (rank == MANAGER)
+        printf("Before for loop, factor = %f.\n", factor);
+
+    long long int chunk = n / size;
+    long long int i_start = rank * chunk;
+    long long int i_end = i_start + chunk;
+    if (rank + 1 == size)
+        i_end += n % size;
+
+    for (i = i_start; i < i_end; i++)
     {
         factor = (i % 2 == 0) ? 1.0 : -1.0;
         sum += factor / (2 * i + 1);
     }
-    printf("After for loop, factor = %f.\n", factor);
 
-    sum = 4.0 * sum;
-    printf("With n = %lld terms\n", n);
-    printf("   Our estimate of pi = %.14f\n", sum);
-    printf("   Ref estimate of pi = %.14f\n", 4.0 * atan(1.0));
+    MPI_Bcast(&factor, 1, MPI_DOUBLE, size - 1, MPI_COMM_WORLD);
+
+    double sum_reduced = 0.0;
+    MPI_Reduce(&sum, &sum_reduced, 1, MPI_DOUBLE, MPI_SUM, MANAGER, MPI_COMM_WORLD);
+
+    if (rank == MANAGER)
+    {
+        printf("After for loop, factor = %f.\n", factor);
+        sum_reduced = 4.0 * sum_reduced;
+        printf("With n = %lld terms\n", n);
+        printf("   Our estimate of pi = %.14f\n", sum_reduced);
+        printf("   Ref estimate of pi = %.14f\n", 4.0 * atan(1.0));
+    }
+
+    MPI_Finalize();
+
     return 0;
 }
 
