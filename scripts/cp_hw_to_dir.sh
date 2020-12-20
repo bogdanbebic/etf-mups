ed5c878907d8603b343a760715dfd5f68bc4857b
Bogdan Bebic <bogdanbebic@gmail.com>
2020-12-20 10:47:00 +0100

    scripts: Add cp_hw_to_dir.sh

---
+++ scripts/cp_hw_to_dir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dst_dir=$1
+
+root_dir=$(realpath `git rev-parse --git-dir`/..)
+
+cp `find $root_dir -name dz?z?.c` $dst_dir
+cp `find $root_dir -name MPS_DZ?_izvestaj_*.pdf` $dst_dir
+cp `find $root_dir -name MPS_DZ?_izvestaj_*.docx` $dst_dir
