2a8e88f4d1d173f525ebde29d81f477f24da02b6
Bogdan Bebic <bogdanbebic@gmail.com>
2021-01-22 11:47:48 +0100

    scripts/cp_hw_to_dir.sh: Add .cu files copy

--- scripts/cp_hw_to_dir.sh
+++ scripts/cp_hw_to_dir.sh
@@ -5,5 +5,6 @@ dst_dir=$1
 root_dir=$(realpath `git rev-parse --git-dir`/..)
 
 cp `find $root_dir -name dz?z?.c` $dst_dir
+cp `find $root_dir -name dz?z?.cu` $dst_dir
 cp `find $root_dir -name MPS_DZ?_izvestaj_*.pdf` $dst_dir
 cp `find $root_dir -name MPS_DZ?_izvestaj_*.docx` $dst_dir
